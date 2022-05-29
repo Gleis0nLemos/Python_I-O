034f81ed1fff31beb92481f4cde5ef8d9477f917
Gleison <gleison04lemos@gmail.com>
2022-05-29 11:48:47 -0300

    Vendo a importância de liberar um arquivo, criando testes de excessões e gerenciando contextos

--- principal.py
+++ principal.py
@@ -1,4 +1,5 @@
-arquivo_contato = open('dados/contatos.csv', encoding='latin_1')
-
-for linha in arquivo_contato:
-    print(linha, end= '')
+for i in xrange(6):
+    with saved(cr):
+        cr.rotate(2 * math.pi * i / 6)
+        cr.rectangle(-25, -60, 50, 40)
+        cr.stroke()
