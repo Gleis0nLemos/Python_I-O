097fde26247d73e24c7723e261a715690b0c6a72
Gleison <gleison04lemos@gmail.com>
2022-05-30 11:23:23 -0300

    Aprendendo a trabalhar com escrita+buffer

--- teste_io.py
+++ teste_io.py
@@ -1,9 +1,11 @@
-arquivo = open('dados/contatos.csv', encoding='latin_1')
+arquivo = open('dados/contatos_escrita.csv', encoding='latin_1', mode='a+')
 
 print(type(arquivo.buffer))
 
 texto_em_bytes = bytes('Esse é um texto em bytes', encoding='latin_1')
-print(texto_em_bytes)
-print(type(texto_em_bytes))
+# print(texto_em_bytes)
+# print(type(texto_em_bytes))
+conteudo = bytes('15,Verônica,veronica@veronica.com.br\n', encoding='latin_1')
+arquivo.buffer.write(conteudo)
 
 arquivo.close()
